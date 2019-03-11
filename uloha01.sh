8ac7da3301f7904bfb495ab54aba3d630152524c
David <davidkostal11@seznam.cz>
2019-03-11 11:03:42 +0100

    changed

--- uloha01.sh
+++ uloha01.sh
@@ -1,10 +1,16 @@
 if [ "$1" ==  "--typ" ]
 then
-  if [ -e $2 ]
+  if [ -e "$2" ]
   then
-   file $2
+    if [ -f "$2" ]
+    then
+    echo "Regular file"
+    else
+    file "$2"
+    fi
   else
-  return 1
+  echo "error - file does not exist"
+  exit 1
   fi
 elif [ "$1" ==  "--help" ]
    then
